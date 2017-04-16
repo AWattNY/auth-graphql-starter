24fee6552edf5667730daec95af1c6f9601655ee
Adam Watt <awattny@me.com>
2017-04-16 06:04:40 -0700

    add SignupForm Component

---
+++ client/components/SignupForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo'; 
+import mutation from '../mutations/Signup';
+import AuthForm from './AuthForm';
+import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
+
+class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    // this.props the old current set of props
+    // nextProps the next set of props that will be in place
+    //when the component rerenders
+    console.log(this.props, nextProps);
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push('/dashboard'); 
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    }).catch(res => {
+      const errors = res.graphQLErrors.map(error => error.message);
+      this.setState({ errors });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sign Up</h3>
+        <AuthForm 
+          errors={[]}
+          onSubmit={this.onSubmit.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+export default graphql(query)(
+  graphql(mutation)(SignupForm)
+);
